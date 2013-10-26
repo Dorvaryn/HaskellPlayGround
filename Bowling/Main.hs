56498da09b592258fe93379f52017e9412f3db9b
Dorvaryn <ben@novoda.com>
2013-10-26 13:30:33 +0100

    Better tests

--- Bowling/Main.hs
+++ Bowling/Main.hs
@@ -1,10 +1,16 @@
 import Bowling
 
-test1 = (score . replicate 20 $ 0) == 0 -- 0
-test2 = (score . replicate 12 $ 10) == 300--300
-test3 = (score $ 10:5:5: (replicate 17 0)) == 30 --30
-test4 = (score $ replicate 20 3) == 60 --60
+testAll :: Int -> Int -> Bool
+testAll each expected = (score . replicate 10 $ each) == expected
+
+tests = [
+         testAll 0 0, 
+         testAll 1 20,
+         (score . replicate 12 $ 10) == 300,
+         (score $ 10:5:5: (replicate 17 0)) == 30,
+         (score $ replicate 20 3) == 60
+        ]
 
 -- Main code
 main :: IO()
-main = print [test1, test2, test3, test4]
+main = print tests
