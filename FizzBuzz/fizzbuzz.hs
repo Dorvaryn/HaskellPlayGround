0c81f4b98c669c280680a605f1e68fb6c21a4f6d
Dorvaryn <ben@novoda.com>
2013-10-26 12:58:58 +0100

    Exclamations

--- FizzBuzz/fizzbuzz.hs
+++ FizzBuzz/fizzbuzz.hs
@@ -6,13 +6,13 @@ iterations = [1..100]
 showFizzBuzz :: (Integral a, Show a) => a -> String
 showFizzBuzz i 
                | i `mod` 3 == 0 = showFizz i
-               | i `mod` 5 == 0 = "Buzz"
+               | i `mod` 5 == 0 = "Buzz!"
                | otherwise = show i
 
 showFizz :: (Integral a, Show a) => a -> String
 showFizz i 
-           | i `mod` 5 == 0 = "FizzBuzz"
-           | otherwise = "Fizz"
+           | i `mod` 5 == 0 = "FizzBuzz!"
+           | otherwise = "Fizz!"
 
 main = do
     putStrLn . unlines $ map showFizzBuzz iterations
