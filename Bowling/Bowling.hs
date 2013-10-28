bdbb0fd9380dce4a3ff2f75eb8bdcacff46e47bf
Dorvaryn <benjamin@odai.homelinux.net>
2013-10-28 15:25:05 +0000

    Better step method

--- Bowling/Bowling.hs
+++ Bowling/Bowling.hs
@@ -9,4 +9,3 @@ score (x:y:z:[]) = x + y + z
 score (x:y:z:xs)   | x == 10 = x + y + z + score (y:z:xs)
                    | (x + y) == 10 = x + y + z + score (z:xs)
                    | otherwise = x + y + score (z:xs)
-
--- GameOfLife/GameOfLife.hs
+++ GameOfLife/GameOfLife.hs
@@ -4,13 +4,24 @@ where
 import Data.List
 import Control.Monad
 
-type Cell = (Int, Int)
+data Status = Dead | Alive deriving (Show, Eq)
+
+type Position = (Integer, Integer)
+type Cell = (Position, Status)
 type World = [Cell]
 
-neighbours :: Cell -> [Cell]
-neighbours (first,second) = [(first+x,second+y) | x <- [-1..1], y <- [-1..1]]
+neighbours :: Cell -> [Position]
+neighbours ((first,second),_) = [(first+x,second+y) | x <- [-1..1], y <- [-1..1]]
 
-numberContained :: [Cell] -> World -> Int
-numberContained (cell:cells) game 
-                                  | elem cell game = 1 + numberContained cells game
-                                  | otherwise = numberContained cells game
+numberAlive :: [Position] -> World -> Int
+numberAlive [] game = 0
+numberAlive (cell:cells) game 
+                                  | elem (cell, Alive) game = 1 + numberAlive cells game
+                                  | otherwise = numberAlive cells game
+
+step :: World -> World -> World
+step [] world = []
+step (cell:cells) world
+                        | (numberAlive (neighbours cell) world == 2) = (fst cell, Alive):(step cells world)
+                        | (numberAlive (neighbours cell) world == 3) = (fst cell, Alive):(step cells world)
+                        | otherwise = (fst cell, Dead):(step cells world)
