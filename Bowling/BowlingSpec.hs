a6961362b4c1c7b12960a7216f98389f0e8d4db2
Dorvaryn <benjamin@odai.homelinux.net>
2013-10-29 12:19:01 +0000

    Cabal package for Bowling kata

--- Bowling/BowlingSpec.hs
+++ Bowling/BowlingSpec.hs
@@ -4,7 +4,10 @@ import Test.Hspec
 import Bowling
 
 main :: IO()
-main = hspec $ do
+main = hspec spec
+
+spec :: Spec
+spec = do
     describe "score" $ do
         it "is 0 for all gutters" $ do
             (score $ replicate 20 0) `shouldBe` 0
