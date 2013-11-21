23f099e007d2f5ef933e448ef0d1f9e51c95dbd2
Dorvaryn <ben@novoda.com>
2013-11-21 22:13:23 +0000

    First iteration

--- RPNSolver/RPNSolver.hs
+++ RPNSolver/RPNSolver.hs
@@ -5,8 +5,8 @@ data Token a = Add | Sub | Mul | Div | Val a deriving Show
 
 type Expression a = [Token a]
 
-solveRPN :: String -> Integer
-solveRPN = solve . readExpression . words
+solveRPN :: String -> Double
+solveRPN xs = solve (readExpression $ words xs) []
 
 readExpression :: [String] -> Expression Integer
 readExpression = map readToken
@@ -18,5 +18,11 @@ readToken "*" = Mul
 readToken "/" = Div
 readToken  v = Val $ read v
 
-solve :: Expression Integer -> Integer
-solve x = 42
+solve :: Expression Integer -> Expression Double -> Double
+solve [] (Val v:[]) = v
+solve (Val v:rest) stack = solve rest (Val (fromInteger v):stack)
+solve (op:rest) (Val x:Val y:stack) = solve rest (Val (y `operation` x):stack)
+    where operation = case op of Add -> (+)
+                                 Sub -> (-)
+                                 Mul -> (*)
+                                 Div -> (/)
